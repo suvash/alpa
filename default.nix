e8669d8b8dbcb8122a05b941de281a943a27979f
Suvash Thapaliya <suvash@gmail.com>
2021-04-22 00:49:55 +0200

    Linter as per nix mode

--- default.nix
+++ default.nix
@@ -1,6 +1,8 @@
 let
   # Pinned nixpkgs, deterministic. Last updated: 2/12/21.
-  pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/a58a0b5098f0c2a389ee70eb69422a052982d990.tar.gz")) {};
+  pkgs = import (fetchTarball
+    ("https://github.com/NixOS/nixpkgs/archive/a58a0b5098f0c2a389ee70eb69422a052982d990.tar.gz"))
+    { };
 in pkgs.mkShell {
   buildInputs = [
     pkgs.cargo
